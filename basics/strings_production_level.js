6792519bcd2d35f053706a842915e055f61516d2
KARTIK PAREEK <115743119+KARTIK614@users.noreply.github.com>
2024-06-16 04:57:35 +0000

    production level string interpolation

---
+++ basics/strings_production_level.js
@@ -0,0 +1,37 @@
+/**
+ * In this file we are practicing the string used in production based on code reusability and effective readability.
+ * 
+ * In the modern development we are using the backticks (`) in writing the js.
+ * it is used for string interpolation in which you have to inject a variable in it, it is kind of similar to the linux shell programming used in the debian based distributions.
+ * 
+ * here is the example of the string interpolation and if you have to print a variable in it then you should use it.
+ */
+let variable = 30;
+console.log(`this is the example of string used in productions with the variable named as ${variable}`);
+// this syntax is more readable and relaible and you can use it more often in the production.
+//this method is known as string interpolation.
+// you can use predefined method to change the output value of the text present in the variable 
+
+//IN IT THE METHOD TO MANUPULATE THE INPUTTED STRING ARE OF A VAST MEJORITY, SO YOU HAVE TO LEARN THEM IN DETAIL.
+// for example
+const gameNews = new String("kartik pareek");
+// this is the method to declare the string in key value pairs not in character array.
+//if you paste this in the console of the chrome and type the variable name after it, it gives you the facility of the see the predefined functions(methods) present in the browser to interact with the text and manupulate it as well.
+// and if you do 
+console.log(gameNews.toUpperCase());// this will call the string by value and print it in capital letter, so that the original string remains untouched.
+// all the predefined functions are not need learn on the go you can see them by this method and gain information regarding them.
+
+
+// for this specific method if you go with the love babber's playlist then you are facing the probleum in developing a good understanding of it because he teaches it like the way of memorization.
+
+
+/**
+ * also see the relevant information regarding the function named as follows:-
+ * substring
+ * slice
+ * trim(used to remove unwanted spaces)
+ * repalce 
+ * includes
+ * split
+ * 
+ */
