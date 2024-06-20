e9b53f58fa46fea1fb40121a93df645f8475d281
KARTIK PAREEK <115743119+KARTIK614@users.noreply.github.com>
2024-06-20 16:13:44 +0000

    scope_basics

---
+++ basics_3/scopes.js
@@ -0,0 +1,13 @@
+/**
+ * scope(game of variable declaration between brackets(paranthesis)).
+ * in it we are studing about the variable validation in code upto a certain level.
+ * it means that the variable is declared in a function can or cannot be used outside of the function (we have to study it) 
+ * var can be used outside of the function and let and const are not permitted to use outside of the code.
+ * thats why the term globle scope and block scope is generated.
+ * 
+ * scope : {}
+ * 
+ * in braces the preferance is given to the inner variable if there is a declaration of that variable outside of the braces of same name.
+ * 
+ * the scope of the browser is differnt from the scope declared in the node enviroment.
+ */
